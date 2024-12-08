6a9881b53738ecddbdb7a86dd22f8bee687a3099
Pranav Setlur <psetlur@gmail.com>
2024-12-08 14:35:27 +0530

    Implemented HuggingFace Login

--- rag_pipeline/rag.py
+++ rag_pipeline/rag.py
@@ -2,12 +2,23 @@ import torch
 from .data_loader import load_data
 from .retrieval import create_retriever
 from .generator import initialize_pipeline, generate_response
+import getpass
+from huggingface_hub import login
 
 SAMPLE_SIZE = 1000
 EMBEDDING_MODEL = "sentence-transformers/all-MiniLM-L6-v2"
 TEXT_COLUMN = "text"
 
+def huggingface_login():
+    print("Please enter your Hugging Face token:")
+    token = getpass.getpass()
+    login(token = token)
+    print("Successfully logged in to Hugging Face.")
+
+
 def main(user_review):
+    #login
+    huggingface_login()
     # load data
     df = load_data(SAMPLE_SIZE)
 
